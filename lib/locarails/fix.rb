1160772d099c83a93d1c6ca5d5e7242e97328939
AkitaOnRails <fabioakita@gmail.com>
2009-03-06 20:31:59 -0300

    fixes

--- lib/locarails/fix.rb
+++ lib/locarails/fix.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-unless Gem.source_index.search('capistrano').select { |g| g.version.to_s =~ /^2\.5/ }.empty?
+unless Gem.source_index.find_name('capistrano').select { |g| g.version.to_s =~ /^2\.5/ }.empty?
   puts "Loading temporary fixes for Capistrano 2.5.x"
   require 'capistrano'
   require 'capistrano/recipes/deploy/dependencies'
